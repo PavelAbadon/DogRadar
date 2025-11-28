ecb86c2d5741ff5797d7883801c4f00380855214
Pavel Vasilev <pavelabadon@gmail.com>
2025-11-28 06:50:36 +0200

    add simple static homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+
 function App() {
     
     return (
         <>
-            <>
-  
-  
-  
-</>
-
+            <Header/>
+            <Home/>
+            <Footer/>
         </>
     )
 }
