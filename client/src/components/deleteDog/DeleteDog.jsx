b636729c092bbc4ed41711f4ed5c0a55dcfa6956
Pavel Vasilev <pavelabadon@gmail.com>
2025-12-02 10:29:52 +0200

    button "Отказ" in delete.jsx work corect

--- client/src/components/deleteDog/DeleteDog.jsx
+++ client/src/components/deleteDog/DeleteDog.jsx
@@ -1,8 +1,19 @@
-import { useNavigate, useParams } from "react-router";
+import { useNavigate, useParams, } from "react-router";
+import { useEffect, useState } from "react";
 
 export default function DeleteDog() {
     const { dogId } = useParams();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [dog, setDog] = useState({});
+
+       useEffect(() =>{
+           (async ()=>{
+               const response = await fetch(`http://localhost:3030/data/dogs/${dogId}`);
+               const result = await response.json();
+               setDog(result);
+               
+           })();
+       },[dogId]);
 
     const deleteDogHandler = async () => {
         try {
@@ -15,26 +26,21 @@ export default function DeleteDog() {
         }        
 
     }
+    //TODO ГОЛЯМО туду нямаме аутотентикация все още и не работи deleteDogHandler. 
+    // Това ще трябва да се оправи като получим токен защото триенето на куче не работи за момента искам да видя дали поне бутона за отказ работи
 
     return (
         <main className="container">
             <div className="pixel-card" style={{ padding: 18, marginTop: 18 }}>
                 <h3>Изтриване на куче</h3>
-                <p className="small">
-                    Сигурни ли сте, че искате да изтриете Рой? Тази операция не може да бъде
-                    отменена.
-                </p>
+                <p className="small">`Сигурни ли сте, че искате да изтриете {dog.name}? Тази операция не може да бъде отменена.`</p>
                 <div className="actions" style={{ marginTop: 12 }}>
                     <button className="pixel-btn" style={{ background: "#ffefef", borderColor: "#d44" }} onClick={deleteDogHandler} >
                         Изтрий
                     </button>
-                    <a
-                        className="pixel-btn"
-                        href="details.html"
-                        style={{ background: "#f6f6f2" }}
-                    >
+                    <button className="pixel-btn"  onClick={() => navigate(`/dogs/${dogId}/details`)} style={{ background: "#f6f6f2" }} >
                         Откажи
-                    </a>
+                    </button>
                 </div>
             </div>
         </main>
