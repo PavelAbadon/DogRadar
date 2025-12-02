a1d67cde45a92468db3368311b27a78a7070c763
Pavel Vasilev <pavelabadon@gmail.com>
2025-12-02 12:41:32 +0200

    add dinamic createDog

--- client/src/components/createDog/CreateDog.jsx
+++ client/src/components/createDog/CreateDog.jsx
@@ -1,16 +1,43 @@
 export default function CreateDog() {
+    const createNewDogHandler = async(event) =>{
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const dogData = Object.fromEntries(formData);
+        dogData.age = Number(dogData.age);
+        //console.log(dogData);
+
+        const response = await fetch('http://localhost:3030/data/dogs',{
+            method: 'POST',
+            headers: {
+                'content-type':'aplication/json'
+            },
+            body: JSON.stringify(dogData),
+        });
+
+        const result = await response.json();
+
+        //todo пак голямо тоду, не работи ауторизацията ще трябва да го оправим
+            
+    }
     return (
         <main className="container">
             <h2 style={{ marginTop: 16 }}>Добави куче</h2>
 
             <div className="form" style={{ maxWidth: 600, marginTop: 12 }}>
-                <form action="#" method="post">
-                    
+                <form action="#" method="post" onSubmit={createNewDogHandler}>
+
                     <label htmlFor="dog-name">Име</label>
                     <input id="dog-name" name="name" type="text" required />
 
                     <label htmlFor="dog-breed">Порода</label>
                     <input id="dog-breed" name="breed" type="text" required />
+
+                    <label>Пол</label>
+                    <label><input type="radio" name="gender" value="male" required /> Мъжко</label>
+                    <label><input type="radio" name="gender" value="female" required /> Женско</label>
+
+                    <label htmlFor="dog-age">Години</label>
+                    <input id="dog-age" name="age" type="number" required />
 
                     <label htmlFor="dog-size">Размер</label>
                     <select id="dog-size" name="size" required>
@@ -28,7 +55,16 @@ export default function CreateDog() {
                     />
 
                     <label htmlFor="dog-notes">Бележки</label>
-                    <textarea id="dog-notes" name="notes" rows={3} />
+                    <textarea id="dog-notes" name="description" rows={3} />
+
+                    <label htmlFor="dog-enemies">Врагове (ID-та, разделени със запетая""Тук още не сме направили функционалноста")</label>
+                    <input
+                        id="dog-enemies"
+                        name="enemies"
+                        type="text"
+                        placeholder="id1, id2, id3"
+                    />
+
 
                     <div className="actions">
                         <button className="pixel-btn" type="submit">
