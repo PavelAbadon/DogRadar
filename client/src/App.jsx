b97225edd193d5ed88bc00330833898f99167b2d
Pavel Vasilev <pavelabadon@gmail.com>
2025-12-11 15:57:44 +0200

    add register tempotstu logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router';
+import { useState } from 'react';
 
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
@@ -14,7 +15,14 @@ import CreateDog from './components/createDog/CreateDog';
 
 
 function App() {
-    
+    const [user, setUser] = useState(null);
+
+    const registerHandler = (email) =>{
+        setUser({
+            email
+        })
+    }
+
     return (
         <>
             <Header/>
@@ -24,7 +32,7 @@ function App() {
                 <Route path='/dogs' element={<Gallery/>}/>
                 <Route path='/calendar' element={<Calendar/>}/>
                 <Route path='/login' element={<Login/>}/>
-                <Route path='/register' element={<Register/>}/>
+                <Route path='/register' element={<Register user={user} onRegister={registerHandler}/>}/>
                 <Route path='/logout' element={<Logout/>}/>
                 <Route path='/dogs/:dogId/details' element={<Details/>}/>
                 <Route path='/dogs/:dogId/delete' element={<DeleteDog/>}/>
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,20 +1,42 @@
-export default function Register() {
+
+export default function Register({
+    user, onRegister
+}) {
+    
+
+    const registerSubmit = (formData) =>{
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirm-password');
+
+        //todo трябва валидация 
+        
+        onRegister({email,});
+
+
+    }
+
     return (
         <main className="container">
             <h2 style={{ marginTop: 16 }}>Register</h2>
+            {user && <h2>вече има юзър 
+                wellcome{user.email}</h2>}
             <div className="form" style={{ maxWidth: 520, marginTop: 12 }}>
-                <form action="#" method="post">
-                    <label htmlFor="username">Username</label>
-                    <input id="username" name="username" type="text" required="" />
+                <form action={registerSubmit} method="post">
                     <label htmlFor="email">Email</label>
                     <input id="email" name="email" type="email" required="" />
                     <label htmlFor="password">Password</label>
                     <input id="password" name="password" type="password" required="" />
                     <label htmlFor="confirm">Confirm password</label>
-                    <input id="confirm" name="confirm" type="password" required="" />
+                    <input id="confirm" name="confirm-password" type="password" required="" />
                     <div className="actions">
                         <button className="pixel-btn" type="submit">
                             Create account
+                        </button>
+                    </div>
+                    <div className="actions">
+                        <button className="pixel-btn" onClick={() => navigate('/login')} >
+                             Вече имаш акаунт тук бутона трябва да се оправи
                         </button>
                     </div>
                 </form>
